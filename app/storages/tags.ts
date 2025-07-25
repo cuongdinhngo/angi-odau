75bdd8dee4c682cd4995203d364c17c65acd81f9
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-25 16:34:55 +0700

    feat: default tags

---
+++ app/storages/tags.ts
@@ -0,0 +1,74 @@
+export const TAGS: Tag[] = [
+  {
+    label: 'Ăn Sáng',
+    value: 'an-sang',
+  },
+  {
+    label: 'Ăn Trưa',
+    value: 'an-trua',
+  },
+  {
+    label: 'Ăn Tối',
+    value: 'an-toi',
+  },
+  {
+    label: 'Ăn Khuya',
+    value: 'an-khuya',
+  },
+  {
+    label: 'Đồ Uống',
+    value: 'do-uong',
+  },
+  {
+    label: 'Ăn Vặt',
+    value: 'an-vat',
+  },
+  {
+    label: 'Đồ Ngọt',
+    value: 'do-ngot',
+  },
+  {
+    label: 'Đồ Chay',
+    value: 'do-chay',
+  },
+  {
+    label: 'Đồ Nướng',
+    value: 'do-nuong',
+  },
+  {
+    label: 'Hàn Quốc',
+    value: 'han-quoc',
+  },
+  {
+    label: 'Nhật Bản',
+    value: 'nhat-ban',
+  },
+  {
+    label: 'Trung Quốc',
+    value: 'trung-quoc',
+  },
+  {
+    label: 'Bún',
+    value: 'bun',
+  },
+  {
+    label: 'Phở',
+    value: 'pho',
+  },
+  {
+    label: 'Cơm',
+    value: 'com',
+  },
+  {
+    label: 'Mì',
+    value: 'mi',
+  },
+  {
+    label: 'Bánh Mì',
+    value: 'banh-mi',
+  },
+  {
+    label: 'Pizza',
+    value: 'pizza',
+  },
+];
