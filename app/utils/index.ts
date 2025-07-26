9ccc610f780574859af4b5b63701c0db88c29e27
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-26 23:15:48 +0700

    feat: getPlacePhotoUrl

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,7 @@
 export function iconPath(icon: string): string {
   return `icons/${icon}`;
 }
+
+export function getPlacePhotoUrl(photo: string): string {
+  return `https://foodtourdanang.vn/${photo}`;
+}
