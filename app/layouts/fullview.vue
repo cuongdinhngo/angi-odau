0cf80dcf5b87063c48151c6df8941a4817a4d368
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-27 20:14:25 +0700

    feat: fullview layout

---
+++ app/layouts/fullview.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <v-row no-gutters>
+          <v-col
+            cols="12"
+          >
+            <slot />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
