c5a5396028faeab23b25e426053cf16f7df599a2
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-28 22:45:21 +0700

    feat: generateUUID

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -5,3 +5,11 @@ export function iconPath(icon: string): string {
 export function getPlacePhotoUrl(photo: string): string {
   return `https://foodtourdanang.vn/${photo}`;
 }
+
+export function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
